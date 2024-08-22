186b69d783afddcafc9a04c9ab2070e813cda501
Faris Hafiz <ahfaris0379@gmail.com>
2024-08-23 06:01:01 +0800

    Delete LearnSphereMobileAppTemplate directory
    Replaced with LearnSphereMobileTemplate

--- LearnSphereMobileAppTemplate/learnsphere/lib/app/modules/editprofile_screen/controllers/editprofile_screen_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-
-class EditprofileScreenController extends GetxController {
-  //TODO: Implement EditprofileScreenController
-
-  final count = 0.obs;
-
-  void increment() => count.value++;
-}
--- LearnSphereMobileAppTemplate/learnsphere/lib/app/modules/get_started/controllers/get_started_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-
-class GetStartedController extends GetxController {
-  //TODO: Implement GetStartedController
-
-  final count = 0.obs;
-
-  void increment() => count.value++;
-}
