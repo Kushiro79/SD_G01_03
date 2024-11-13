2a82acdfaf17f25297d22c66fcf5bb48c103dcf6
Faris Hafiz <ahfaris0379@gmail.com>
2024-11-13 21:20:30 +0800

    File organization

--- LearnSphereAppTemplate/lib/app/modules/createpin_screen/controllers/createpin_screen_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-
-class CreatepinScreenController extends GetxController {
-  //TODO: Implement CreatepinScreenController
-
-  final count = 0.obs;
-
-  void increment() => count.value++;
-}
